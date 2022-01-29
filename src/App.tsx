a98bc9a1cdb006280d28346155682ad0b583ec01
Aran <aran@ermarian.net>
2022-01-29 12:20:44 +0100

    Remove logo, change branding and remove unused files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,6 @@
 import React from 'react';
-import { useEffect, useState } from "react";
 import './App.css';
 import Game from './Game';
-import { maxGuesses } from "./util";
-import {ReactComponent} from "*.svg";
-
-function useSetting<T>(
-  key: string,
-  initial: T
-): [T, (value: T | ((t: T) => T)) => void] {
-  const [current, setCurrent] = useState<T>(() => {
-    try {
-      const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : initial;
-    } catch (e) {
-      return initial;
-    }
-  });
-  const setSetting = (value: T | ((t: T) => T)) => {
-    try {
-      const v = value instanceof Function ? value(current) : value;
-      setCurrent(v);
-      window.localStorage.setItem(key, JSON.stringify(v));
-    } catch (e) {}
-  };
-  return [current, setSetting];
-}
 
 class App extends React.Component {
   componentDidMount(){
